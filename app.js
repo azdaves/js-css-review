c16c1a97956739964eb6c0cea426962baf6db79c
Dave Schilling <ds@Daves-MacBook-Pro.local>
2018-12-04 12:00:00 -0700

    initial files

---
+++ app.js
@@ -0,0 +1,14 @@
+const bodyEl = document.querySelector("body");
+const divEl = document.querySelector("div");
+
+
+bodyEl.style.margin = "0";
+bodyEl.style.padding = "0";
+bodyEl.style.backgroundColor = "#f0f0f2";
+bodyEl.style.fontFamily = "sans-serif";
+
+divEl.style.margin = "5em, auto";
+divEl.style.width = "600px";
+divEl.style.padding = "50px";
+divEl.style.backgroundColor = "#fff";
+divEl.style.borderRadius = "1em";
